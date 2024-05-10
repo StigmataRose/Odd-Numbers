3ea47aa76451ad562bbe79e18378c89c9c506532
skyler@typewriteraudio.com <skyler@typewriteraudio.com>
2024-05-10 07:59:25 -0500

    Reverse List
    Reversed List

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 #For the record I did not use chatgpt for this
 odd_numbers = []
 
-isOdd = True
+is_odd = True
 
 for i in range(1,100):
-    if isOdd: 
+    if is_odd: 
         odd_numbers.append(i)
     
-    isOdd = not isOdd
+    is_odd = not is_odd
 
+def reverseList(lst):
+    return lst[::-1]
 
+print("Odd Numbers")
 print(odd_numbers)
+
+print("Reversed Odd Numbers")
+reverse_odd_numbers = reverseList(odd_numbers)
+print(reverse_odd_numbers)
