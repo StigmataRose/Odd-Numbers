eb40a0789a0180788d28bf4fe3e9c7e4c148b407
skyler@typewriteraudio.com <skyler@typewriteraudio.com>
2024-05-15 14:57:10 -0500

    with testing

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Odd-Numbers
-simple script that prints odd numbers between 1 - 100
+simple script that prints odd numbers between 1 - 100,
+then reverse the list.
+
+The latest version calculates the most optimized way to gather these numbers.
 
 ```
 python3 main.py
--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-#For the record I did not use chatgpt for this
-odd_numbers = []
+import time
 
+max_value = 100
+
+# start timer 1
+start_time_1 = time.time_ns()
+#---------------------------------------------------------------
+odd_numbers_1 = []
 is_odd = True
-
-for i in range(1,100):
+for i in range(1,max_value):
     if is_odd: 
-        odd_numbers.append(i)
+        odd_numbers_1.append(i)
     
     is_odd = not is_odd
 
-def reverseList(lst):
-    return lst[::-1]
+reverse_odd_numbers_1 = odd_numbers_1[::-1]
+#---------------------------------------------------------------
+#end of timer 1
+end_time_1 = time.time_ns()
+# Calculate the elapsed time
+elapsed_time_1 = end_time_1 - start_time_1
 
-print("Odd Numbers")
-print(odd_numbers)
 
-print("Reversed Odd Numbers")
-reverse_odd_numbers = reverseList(odd_numbers)
-print(reverse_odd_numbers)
+# start timer 2
+start_time_2 = time.time_ns()
+#---------------------------------------------------------------
+odd_numbers_2 = []
+reverse_odd_numbers_2 = []
+for i in range(1,max_value,2):
+    odd_numbers_2.append(i)
+    reverse_odd_numbers_2.append(max_value-i)
+#---------------------------------------------------------------
+#end of timer 2
+end_time_2 = time.time_ns()
+
+# Calculate the elapsed time
+elapsed_time_2 = end_time_2 - start_time_2
+print("Elapsed time (Approach 1):", elapsed_time_1, "nano-seconds")
+print("Elapsed time (Approach 2):", elapsed_time_2, "nano-seconds")
+percent_increase = (elapsed_time_2 / elapsed_time_1) * 100
+print("Percent Increase: ", round(percent_increase,2), " %")
